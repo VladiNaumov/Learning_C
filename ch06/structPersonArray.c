a449ddb494980c3b970d9365b0f9f16fe50ccad6
vov4.games@yandex.ru <HostG3$Hk8@>
2024-06-28 17:57:16 +0300

    refactoring

--- ch06/structPersonArray.c
+++ ch06/structPersonArray.c
@@ -2,15 +2,15 @@
 #include <stdlib.h>
 #include <string.h>
 
-// Определение структуры Person
-struct Person {
+// Определение структуры Person через typedef
+typedef struct {
     char name[50];
     int age;
-};
+} Person;
 
 int main() {
     int numPersons = 5; // Количество элементов в массиве
-    struct Person *persons = (struct Person *)malloc(numPersons * sizeof(struct Person));
+    Person *persons = (Person *)malloc(numPersons * sizeof(Person));
 
     // Проверка, выделена ли память успешно
     if (persons == NULL) {
@@ -34,6 +34,14 @@ int main() {
     strcpy(persons[4].name, "Алексей Васильев");
     persons[4].age = 40;
 
+    /*
+    
+    Инициализация данных:
+    Каждый элемент массива persons инициализируется с помощью функции strcpy, 
+    которая копирует строку имени в поле name, а затем устанавливает возраст в поле age.
+    
+    */
+
     // Пример обращения к элементам массива объектов Person
     for (int i = 0; i < numPersons; ++i) {
         printf("Person %d: %s, возраст %d лет\n", i+1, persons[i].name, persons[i].age);
@@ -44,3 +52,4 @@ int main() {
 
     return 0;
 }
+
