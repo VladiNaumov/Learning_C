5f616bacbe0366f8b90a8163824344e30117f757
vov4.games@yandex.ru <HostG3$Hk8@>
2024-06-28 18:02:11 +0300

    refactoring

--- ch06/structPersonArray.c
+++ ch06/structPersonArray.c
@@ -8,17 +8,17 @@ typedef struct {
     int age;
 } Person;
 
-int main() {
-    int numPersons = 5; // Количество элементов в массиве
+// Функция для выделения памяти под массив структур Person
+Person* allocatePersonsArray(int numPersons) {
     Person *persons = (Person *)malloc(numPersons * sizeof(Person));
-
-    // Проверка, выделена ли память успешно
     if (persons == NULL) {
         fprintf(stderr, "Ошибка выделения памяти\n");
-        return 1;
     }
+    return persons;
+}
 
-    // Пример инициализации массива объектов Person
+// Функция для инициализации массива объектов Person
+void initializePersons(Person *persons, int numPersons) {
     strcpy(persons[0].name, "Иван Иванов");
     persons[0].age = 30;
 
@@ -35,20 +35,44 @@ int main() {
     persons[4].age = 40;
 
     /*
-    
+
     Инициализация данных:
     Каждый элемент массива persons инициализируется с помощью функции strcpy, 
     которая копирует строку имени в поле name, а затем устанавливает возраст в поле age.
     
     */
+}  
 
-    // Пример обращения к элементам массива объектов Person
+// Функция для вывода информации о каждом объекте Person
+void printPersons(const Person *persons, int numPersons) {
     for (int i = 0; i < numPersons; ++i) {
-        printf("Person %d: %s, возраст %d лет\n", i+1, persons[i].name, persons[i].age);
+        printf("Person %d: %s, возраст %d лет\n", i + 1, persons[i].name, persons[i].age);
+    }
+}
+
+// Функция для освобождения памяти, выделенной под массив Person
+void freePersonsArray(Person *persons) {
+    free(persons);
+}
+
+
+int main() {
+    int numPersons = 5; // Количество элементов в массиве
+
+    // Выделение памяти под массив объектов Person
+    Person *persons = allocatePersonsArray(numPersons);
+    if (persons == NULL) {
+        return 1; // В случае ошибки выделения памяти завершаем программу
     }
 
-    // Освобождение памяти после использования
-    free(persons);
+    // Инициализация массива объектов Person
+    initializePersons(persons, numPersons);
+
+    // Вывод информации о каждом объекте Person
+    printPersons(persons, numPersons);
+
+    // Освобождение памяти, выделенной под массив объектов Person
+    freePersonsArray(persons);
 
     return 0;
 }
