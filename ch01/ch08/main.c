30c8260704a0f7f7280d6fffd5d34f0df67cdc34
umbre <umbrella.spb@hotmail.com>
2024-08-23 13:35:23 +0300

    refactoring

--- ch01/ch08/main.c
+++ ch01/ch08/main.c
@@ -9,7 +9,7 @@ typedef struct {
 } Rectangle;
 
 // Функция для создания прямоугольника и вычисления его свойств
-Rectangle create_rectangle(Rectangle rect, float width, float height) {
+Rectangle create_rectangle(Rectangle *rect, float width, float height) {
     rect.width = width;            // Устанавливаем ширину
     rect.height = height;          // Устанавливаем высоту
     rect.area = width * height;    // Вычисляем площадь
@@ -40,7 +40,7 @@ int main() {
 
      Rectangle rec;
     // Создание прямоугольника через функцию create_rectangle
-    Rectangle myRect = create_rectangle(rec, 5.0, 3.0);
+    Rectangle myRect = create_rectangle(&rec, 5.0, 3.0);
 
     // Вывод свойств прямоугольника на экран
     print_rectangle(myRect);
@@ -48,51 +48,6 @@ int main() {
     return 0;
 }
 
-/*   способ второй   */
-
-// Определение структуры Rectangle
-typedef struct {
-    float width;      // Ширина
-    float height;     // Высота
-    float area;       // Площадь
-    float perimeter;  // Периметр
-} Rectangle;
-
-// Функция для создания прямоугольника и вычисления его свойств
-Rectangle create_rectangle(Rectangle *rect, float width, float height) {
-    rect->width = width;            // Устанавливаем ширину
-    rect->height = height;          // Устанавливаем высоту
-    rect->area = width * height;    // Вычисляем площадь
-    rect->perimeter = 2 * (width + height); // Вычисляем периметр
-    return *rect;                   // Возвращаем заполненную структуру
-}
-
-/*
-    Передача указателя на структуру
-    Когда ты передаёшь указатель на структуру, ты передаёшь адрес оригинальной структуры.
-    Это позволяет функции напрямую изменять структуру, поскольку она работает с оригинальными данными, а не с их копией.
-*/
-
-// Функция для вывода свойств прямоугольника
-void print_rectangle(Rectangle rect) {
-    printf("Rectangle:\n");
-    printf("Width: %.2f\n", rect.width);
-    printf("Height: %.2f\n", rect.height);
-    printf("Area: %.2f\n", rect.area);
-    printf("Perimeter: %.2f\n", rect.perimeter);
-}
-
-int main() {
-
-    Rectangle *rec;
-    // Создание прямоугольника через функцию create_rectangle
-    Rectangle myRect = create_rectangle(rec, 5.0, 3.0);
-
-    // Вывод свойств прямоугольника на экран
-    print_rectangle(myRect);
-
-    return 0;
-}
 
 /*
 Основные различия
